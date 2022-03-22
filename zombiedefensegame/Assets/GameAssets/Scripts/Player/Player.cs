f8563a238cbeea27557b22f6485194ec897dc0ee
AAskin <abdullahahmetaskin@gmail.com>
2022-03-22 11:28:34 +0000

    Modified player

--- zombiedefensegame/Assets/GameAssets/Scripts/Player/Player.cs
+++ zombiedefensegame/Assets/GameAssets/Scripts/Player/Player.cs
@@ -4,7 +4,11 @@ namespace TheyAreComing
 {
     public class Player : MonoBehaviour
     {
+        [SerializeField] private CharacterSettingsScriptableObject characterSettingsScriptableObject;
         private PlayerStateManager _playerStateManager;
+
+        public PlayerCharacterSettings PlayerCharacterSettings =>
+            characterSettingsScriptableObject.PlayerCharacterSettings;
 
         private PlayerStateManager playerStateManager => _playerStateManager
             ? _playerStateManager
