871134ab95fa5292ea8601a86ddf760c0a295130
AAskin <abdullahahmetaskin@gmail.com>
2022-04-04 17:38:36 +0100

    Modified managers

---
+++ zombiedefensegame/Assets/GameAssets/Scripts/TransformDynamicPosition.cs
@@ -0,0 +1,34 @@
+﻿using AmazingAssets.CurvedWorld;
+using UnityEngine;
+
+namespace TheyAreComing
+{
+    [ExecuteAlways]
+    public class TransformDynamicPosition : MonoBehaviour
+    {
+        public Transform parent;
+        public Vector3 offset;
+        public bool recalculateRotation;
+        private CurvedWorldController _curvedWorldController;
+
+        private void Start()
+        {
+            _curvedWorldController = GameManager.CurvedWorldController;
+            if (parent == null)
+                parent = transform.parent;
+        }
+
+        private void Update()
+        {
+            //Transform position
+            transform.position = _curvedWorldController.TransformPosition(parent.position + offset);
+
+
+            //Transform normal (calcualte rotation)
+            if (recalculateRotation)
+                transform.rotation =
+                    _curvedWorldController.TransformRotation(parent.position + offset, parent.forward,
+                        parent.right);
+        }
+    }
+}
